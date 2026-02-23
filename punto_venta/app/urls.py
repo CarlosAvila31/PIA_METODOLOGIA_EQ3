55759e0b8fe3c3236e3673da2063fc8648a6145d
CarlosAvila31 <carrosavila@gmail.com>
2026-02-22 19:11:45 -0600

    Añadí un login provisional, tendriamos que ver si separar views por tipo de usuario

--- punto_venta/app/urls.py
+++ punto_venta/app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
+from .views import CustomLoginView
 
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),  
+    path('', views.inicio, name='inicio'), 
+    path('login/', CustomLoginView.as_view(), name='login'), 
+    path('listar/', views.listar_reservaciones, name='listar'),
 ]
--- punto_venta/app/views.py
+++ punto_venta/app/views.py
@@ -1,7 +1,60 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Categoria, Producto, Empleado, Cliente, Venta, DetalleVenta, Caja, Proveedor, Compra, DetalleCompra, CuentaPorPagar
+from django.contrib.auth.views import LoginView
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from datetime import datetime, timedelta
+from django.utils import timezone 
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.models import Group, User
+from django.db import models
+
 
 
 # Create your views here.
 def inicio(request):
     return render(request, 'index.html')
+
+
+class RoleLoginForm(AuthenticationForm):
+    role = forms.ChoiceField(
+        choices=[('admin', 'Administrador'), ('empleado', 'Empleado')],
+        label='Tipo de usuario'
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['role'].widget.attrs.update({'id': 'id_role'})
+
+def is_admin(user):
+    return user.is_superuser
+
+def is_empleado(user):
+    return user.groups.filter(name='Empleados').exists()
+
+@login_required
+@user_passes_test(is_admin)
+def listar_reservaciones(request):
+    productos = Producto.objects.all()
+    return render(request, 'listar.html', {'productos': productos})
+
+@login_required
+@user_passes_test(is_empleado)
+def listar_empleado(request):
+    productos = Producto.objects.all()
+    return render(request, 'listar.html', {'productos': productos})
+
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    authentication_form = RoleLoginForm
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = form.get_user()
+        
+        if user.is_superuser:
+            return redirect('listar')
+        elif is_empleado(user):
+            return redirect('listar_empleado')
+        return response
