c29092b8e9b90e8bc7ca62c34400aafaaa2f54d4
CarlosAvila31 <carrosavila@gmail.com>
2026-02-22 18:16:10 -0600

    Agrege una plantilla html temporal para comprobar que este bien configurado el static

--- punto_venta/app/views.py
+++ punto_venta/app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Categoria, Producto, Empleado, Cliente, Venta, DetalleVenta, Caja, Proveedor, Compra, DetalleCompra, CuentaPorPagar
+
 
 # Create your views here.
+def inicio(request):
+    return render(request, 'index.html')
