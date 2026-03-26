e5f14e899859d53157cd52346c1dfa98b9321a43
CarlosAvila31 <carrosavila@gmail.com>
2026-03-26 00:27:12 -0600

    Ya se conecta el carrito al registro de ventas VAMOSS CHAVAAAL

--- punto_venta/app/urls.py
+++ punto_venta/app/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('listar_empleado/', views.listar_empleado, name='listar_empleado'),
     path('listado_ventas/', views.listado_ventas, name='listado_ventas'),
     path('eliminar/<int:id>/', views.eliminar_venta, name='eliminar'),
+    path('guardar-venta/', views.guardar_venta, name='guardar_venta'),
 ]
--- punto_venta/app/views.py
+++ punto_venta/app/views.py
@@ -1,3 +1,5 @@
+import json
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Categoria, Producto, Empleado, Cliente, Venta, Caja, Proveedor, Compra, DetalleCompra, CuentaPorPagar, DetalleVenta
 from django.contrib.auth.views import LoginView
@@ -12,8 +14,6 @@ from django.db import models
 
 
 # Create your views here.
-#def inicio(request):
-    #return render(request, 'login.html')
 
 
 class RoleLoginForm(AuthenticationForm):
@@ -71,3 +71,65 @@ def eliminar_venta(request, id):
     venta = get_object_or_404(Venta, id=id)
     venta.delete()
     return redirect('listado_ventas')
+
+
+@login_required
+def guardar_venta(request):
+    if request.method == "POST":
+
+        data = json.loads(request.body)
+
+        carrito = data.get("carrito", [])
+        metodo_pago = data.get("metodo_pago")
+        nombre = data.get("nombre")
+        apellido = data.get("apellido")
+
+        # ==========================
+        # EMPLEADO (DESDE USER)
+        # ==========================
+        empleado = request.user.empleado
+
+        # ==========================
+        # CLIENTE (OPCIONAL)
+        # ==========================
+        cliente = None
+        if nombre:
+            cliente = Cliente.objects.create(
+                nombre=nombre,
+                primer_apellido=apellido
+            )
+
+        # ==========================
+        # CREAR VENTA
+        # ==========================
+        venta = Venta.objects.create(
+            fecha_hora=timezone.now(),
+            metodo_pago=metodo_pago,
+            empleado=empleado,
+            cliente=cliente
+        )
+
+        total = 0
+
+        # ==========================
+        # DETALLE VENTA
+        # ==========================
+        for item in carrito:
+
+            producto = Producto.objects.get(id=item["id"])
+
+            detalle = DetalleVenta.objects.create(
+                venta=venta,
+                producto=producto,
+                cantidad=item["cantidad"]
+            )
+
+            total += detalle.subtotal
+
+        # ==========================
+        # ACTUALIZAR TOTAL
+        # ==========================
+        venta.total = total
+        venta.save()
+
+        return JsonResponse({"status": "ok"})
